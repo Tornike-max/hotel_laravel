f803805861f496b9b653844d616a1d1ad2ac92ad
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-09-01 01:00:53 +0400

    added tables

---
+++ backend/database/migrations/2024_08_31_204322_create_careers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Company;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->longText('description');
+            $table->foreignIdFor(Company::class);
+            $table->text('location');
+            $table->text('salary');
+            $table->text('employment_type');
+            $table->text('experience_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('careers');
+    }
+};
